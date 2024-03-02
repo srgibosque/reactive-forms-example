70e45ee1e1276927b855e47316821aa89633eac3
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-03-02 15:50:38 +0100

    strict property initialization set to true

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+import { UserDTO } from '../../models/user.dto';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  user: UserDTO;
+  email: FormControl;
+  password: FormControl;
+  loginForm: FormGroup;
 
+
+  constructor(private formBuilder: FormBuilder){
+    this.user = new UserDTO('', '');
+
+    this.email = new FormControl(this.user.email);
+
+    this.password = new FormControl(this.user.password);
+
+    this.loginForm = this.formBuilder.group({
+      email: this.email,
+      password: this.password
+    });
+  }
+
+  onSubmit(){
+    this.user.email = this.email.value;
+    this.user.password = this.password.value;
+
+    console.log('User: ' + this.user.email);
+    console.log('Password: ' + this.user.password);
+  }
 }
